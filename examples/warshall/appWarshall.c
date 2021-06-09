da3f8cd8d54da7cf4bdb1c0198a78e08d7aed370
Hermes Senger <senger.hermes@gmail.com>
2021-06-09 15:58:30 -0300

    fix warshall

--- examples/warshall/appWarshall.c
+++ examples/warshall/appWarshall.c
@@ -4,48 +4,63 @@
 
 #define QTD_ELEMENTOS 1024
 
+void imprimeSoma(int *data, unsigned n)
+{
+    double soma = 0;
+    for (int i=0; i < n; i++) {
+				for (int j=0; j < n; j++){
+					soma += data[i * n + j];		
+				}	    
+	  } 
+    printf("A soma é %d\n",soma);
+}
+
 void inicializaMatriz(int *data, unsigned size)
 {
   time_t t;
 	srand((unsigned int) time(&t));
 	for (int i=0; i<size; i++) {
-     for (int j=0; j<size; j++) {
+     for (int j=0; j<size; j++) { 
 		   //data[i * size + j] = (int)( rand() & 0xFF )/10.0f;
-			 data[i * size + j] = ((int)rand() ) % 2;
-     }
+			 data[i * size + j] = ((int)rand() ) % 2;			 
+     }  
 	}
 }
 
 
-void warshallCPU(int* A, int* F, unsigned n)
-{
+void warshallCPU(int* fechoMatriz, unsigned n)
+{		
 		for(int k = 0; k < n; k++){
-			for(int lin = 0; lin < n; lin ++){
-				for(int col = 0; col < n; col ++){
-						if(A[k * n + col] == 1 && A[lin * n + k] == 1)
-							F[lin * n + col] = 1;
-				}
-			}
+			for(int i = 0; i < n; i++){
+				for(int j = 0; j < n; j++){
+						if(fechoMatriz[k * n + j] == 1 && fechoMatriz[i * n + k] == 1)	
+							fechoMatriz[i * n + j] = 1;
+				}			
+			}					
 		}
 }
-
 
 void processamentoCPU(int *A, unsigned n)
 {
 	int* F = (int*) malloc( sizeof(int) * n * n);
+  
+  memcpy(F, A, sizeof(int)*n*n);
 
   double tempoGasto;
 	clock_t start = clock();
 
-	warshallCPU(A, F, n);
-
+	warshallCPU(F, n);
+	
 	clock_t stop = clock();
-
-	tempoGasto = 1000 *  (stop - start) / (float) CLOCKS_PER_SEC;
-	printf("Tempo de execução da CPU: %f ms\n", tempoGasto );
+	
+	tempoGasto = (stop - start) / (float) CLOCKS_PER_SEC;
+ 
+	printf("Tempo de execução da CPU: %f s\n", tempoGasto ); 
+  imprimeSoma(F, n);
 
 	free(F);
 }
+
 
 void mainWarshall()
 {
@@ -53,17 +68,11 @@ void mainWarshall()
 	int byteNumber = QTD_ELEMENTOS * QTD_ELEMENTOS * sizeof(int);
 
 	int *A = (int*) malloc(byteNumber);
-
+	
 	inicializaMatriz(A, QTD_ELEMENTOS);
 	processamentoCPU(A, QTD_ELEMENTOS);
 
-  int soma = 0;
-  for (int i=0; i< QTD_ELEMENTOS; i++)
-    for (int j=0; j< QTD_ELEMENTOS; j++)
-      soma += A[i * QTD_ELEMENTOS + j];
-  printf("Total de arestas: %d \n ",soma);
-  
-	free(A);
+	free(A);	
 }
 
 int main(void)
@@ -71,3 +80,4 @@ int main(void)
 	mainWarshall();
 	return 0;
 }
+
